a48fde1e92cbd6c89cfcc776e24fa34b1d07f210
User <User@WIN-D6I7BGDFHRA>
2022-05-10 19:48:42 +0200

    Добавьте файлы проекта.

---
+++ HomeWork 050.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    double FirstNumber;
+    std::cout << "First number = ";
+    std::cin >> FirstNumber;
+
+
+    double SecondNumber;
+    std::cout << "Second number = ";
+    std::cin >> SecondNumber;
+
+
+    int SumOfIntegers = FirstNumber + SecondNumber;
+    float SumOfFractions = FirstNumber + SecondNumber - SumOfIntegers;
+
+    std::cout << "Sum of integers = " << SumOfIntegers << "\n";
+    std::cout << "Sum of fractions = " << SumOfFractions * 100 << " or " << SumOfFractions << "\n";
+}
