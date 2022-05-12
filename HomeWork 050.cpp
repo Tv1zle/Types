16728368a413622a6095ce050404114cb6ccd532
Tv1zle <103439191+Tv1zle@users.noreply.github.com>
2022-05-12 16:24:15 +0200

    Update HomeWork 050.cpp

--- HomeWork 050.cpp
+++ HomeWork 050.cpp
@@ -38,3 +38,27 @@ int main()
 
     std::cout << "You must go at speed = " << Distance / Time << " (KM/H)";
 }
+
+//Task 3
+
+#include <iostream>
+
+int main()
+{
+    double TimeInSeconds;
+    std::cout << "Time elapsed since the beginning of the working day (SEC) = ";
+    std::cin >> TimeInSeconds;
+
+
+    double AllTimeForOneDay = 8 * 60 * 60; //Так мы узнаем время всего рабочего дня в секундах.
+
+
+    if (TimeInSeconds >= AllTimeForOneDay) {
+        std::cout << "Go home, stalker.";
+    }
+    else {
+        std::cout << "You have left = " << (AllTimeForOneDay - TimeInSeconds) / 60 / 60 << "\n";
+    }
+}
+
+//Task 4
