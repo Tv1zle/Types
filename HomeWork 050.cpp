e5b812be0b5ea41e8d68e79615aba070dff1f30a
Tv1zle <103439191+Tv1zle@users.noreply.github.com>
2022-05-12 16:25:06 +0200

    Update HomeWork 050.cpp

--- HomeWork 050.cpp
+++ HomeWork 050.cpp
@@ -1,4 +1,5 @@
-﻿
+//Task 1
+
 #include <iostream>
 
 int main()
@@ -87,3 +88,31 @@ int main()
 }
 
 // Task 5
+
+
+#include <iostream>
+
+int main()
+{
+    double SizeFilm;
+    std::cout << "Size of film (GB) = ";
+    std::cin >> SizeFilm;
+
+
+    double SpeedOfInternet;
+    std::cout << "Speed of internet connection (B/S) = ";
+    std::cin >> SpeedOfInternet;
+
+
+    SizeFilm = SizeFilm * 1024 * 8;
+    SizeFilm /= SpeedOfInternet;
+
+
+    int Hours = SizeFilm / 3600;
+    int Minutes = SizeFilm / 60 - Hours * 60;
+    int Seconds = SizeFilm - Minutes * 60 - Hours * 3600;
+
+
+    std::cout << "Movie download for " << Hours << " hours " << Minutes << " minutes " << Seconds << " seconds.";
+
+}
