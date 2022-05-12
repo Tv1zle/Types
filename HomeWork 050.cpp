e3b4a855b139d5c80ff0a062819935d8b25d15db
Tv1zle <103439191+Tv1zle@users.noreply.github.com>
2022-05-12 16:24:41 +0200

    Update HomeWork 050.cpp

--- HomeWork 050.cpp
+++ HomeWork 050.cpp
@@ -62,3 +62,28 @@ int main()
 }
 
 //Task 4
+
+
+#include <iostream>
+
+int main()
+{
+    double LaptopCost;
+    std::cout << "Laptop cost = ";
+    std::cin >> LaptopCost;
+
+
+    double LaptopQuantity;
+    std::cout << "Laptop quantity = ";
+    std::cin >> LaptopQuantity;
+
+
+    double LaptopPercentOfDiscount;
+    std::cout << "Laptop percent of discount = ";
+    std::cin >> LaptopPercentOfDiscount;
+
+
+    std::cout << "Total amount = " << LaptopCost * LaptopQuantity / 100 * LaptopPercentOfDiscount - 0.01;
+}
+
+// Task 5
